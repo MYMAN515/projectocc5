f31033239cbff996afc169e6a80616b79cf0c6b6
MYMAN515 <dr.rakradi@gmail.com>
2026-01-11 18:47:24 +0800

    Add localized brush teeth modal images

--- script.js
+++ script.js
@@ -495,8 +495,16 @@ const content = {
                         text: "My Morning Hygiene Steps",
                         type: "strip",
                         steps: [
-                            { image: "https://img.freepik.com/premium-photo/asian-man-waking-up-morning-sitting-bed-stretching_126277-1356.jpg", label: "Wake Up" },
-							{ image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDg1djlmZ2Zna213dGd6amEwNHJjdHNoNWZlcTJ4bTF1Y203c3cxOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YaY0FF0eUWLtVNK1Uw/giphy.gif", label: "Brush Teeth" },
+							{ image: "https://img.freepik.com/premium-photo/asian-man-waking-up-morning-sitting-bed-stretching_126277-1356.jpg", label: "Wake Up" },
+							{
+                                image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDg1djlmZ2Zna213dGd6amEwNHJjdHNoNWZlcTJ4bTF1Y203c3cxOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YaY0FF0eUWLtVNK1Uw/giphy.gif",
+                                label: "Brush Teeth",
+                                modalImages: {
+                                    en: "images/be.jpg",
+                                    ms: "images/bm.jpg",
+                                    zh: "images/bc.jpg"
+                                }
+                            },
                             {
                                 image: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGJqbDBocDk2NWs1eGNueGNrcW13M2cxYXE5M3MzcThkcmc4bWY2OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Ri8FpKsdTwiaUFWysZ/giphy.gif",
                                 label: "Wash Face",
